eea30a8a0a66a61b483d7718dc5f2758d8d0177e
sixie <si.xie@cern.ch>
2022-03-18 20:41:33 +0100

    update for EIC 2022 sensor

--- macros/PlotTimeDiffVsXY.py
+++ macros/PlotTimeDiffVsXY.py
@@ -27,6 +27,7 @@ all_histoInfos = [
     HistoInfo("timeDiff_vs_xy_channel03",inputfile, "channel_4"),
     HistoInfo("timeDiff_vs_xy_channel04",inputfile, "channel_5"),
     HistoInfo("timeDiff_vs_xy_channel05",inputfile, "channel_6"),
+    HistoInfo("timeDiff_vs_xy_channel06",inputfile, "channel_7"),
     HistoInfo("timeDiff_vs_xy", inputfile, "time_diff"),
     HistoInfo("timeDiff_vs_xy_amp2", inputfile, "time_diff_amp2"),
     HistoInfo("timeDiff_vs_xy_amp3", inputfile, "time_diff_amp3"),
@@ -47,7 +48,7 @@ for i in range(0, all_histoInfos[0].th2.GetXaxis().GetNbins()+1):
         ##For Debugging
         #if not (i==46 and j==5):
         #    continue
-
+        
         for info in all_histoInfos:
             tmpHist = info.th3.ProjectionZ("pz",i,i,j,j)
             myMean = tmpHist.GetMean()
@@ -55,46 +56,52 @@ for i in range(0, all_histoInfos[0].th2.GetXaxis().GetNbins()+1):
             nEvents = tmpHist.GetEntries()
             fitlow = myMean - 1.5*myRMS
             fithigh = myMean + 1.5*myRMS
-            value = myRMS
+            value = myMean
             error = 0.0
-
+            
             #Do fit 
             if(nEvents > 50):
-                tmpHist.Rebin(4)
-
-                fit = TF1('fit','gaus',fitlow,fithigh)
-                tmpHist.Fit(fit,"Q", "", fitlow, fithigh)
-                myMPV = fit.GetParameter(1)
-                mySigma = fit.GetParameter(2)
-                mySigmaError = fit.GetParError(2)
-                value = 1000.0*mySigma
-                error = 1000.0*mySigmaError
-                        
+                #tmpHist.Rebin(4)
+                
+                # fit = TF1('fit','gaus',fitlow,fithigh)
+                # tmpHist.Fit(fit,"Q", "", fitlow, fithigh)
+                # myMean = fit.GetParameter(1)
+                # mySigma = fit.GetParameter(2)
+                # mySigmaError = fit.GetParError(2)
+                # #value = 1000.0*mySigma
+                # #error = 1000.0*mySigmaError
+                # value = 1000.0*myMean
+                # error = 0
+                
+                
+                
                 ##For Debugging
                 #tmpHist.Draw("hist")
                 ##myLanGausFunction.Draw("same")
                 #fit.Draw("same")
                 #canvas.SaveAs("q_"+str(i)+"_"+str(j)+".gif")
                 #
-                #print ("Bin : " + str(i) + " , " + str(j) + " -> " + str(value))
+                print ("Bin : " + str(i) + " , " + str(j) + " -> " + str(value))
             else:
-                value = 0.0            
-
+                value = -100.0            
+                
             info.th2.SetBinContent(i,j,value)
             info.th2.SetBinError(i,j,error)
-                        
+            #print("Bin " + str(i) + " " + str(j) + " " + str(value))
+                
+
 # Plot 2D histograms
 outputfile = TFile("plots.root","RECREATE")
 for info in all_histoInfos:
     info.th2.Draw("colz")
     info.th2.SetStats(0)
     info.th2.SetTitle(info.outHistoName)
-    info.th2.SetMinimum(0.0)
-    info.th2.SetMaximum(100.0)
+    info.th2.SetMinimum(-1.2)
+    info.th2.SetMaximum(-0.5)
     info.th2.SetLineColor(kBlack)
 
-    canvas.SaveAs("TimeRes_vs_xy_"+info.outHistoName+".gif")
-    canvas.SaveAs("TimeRes_vs_xy_"+info.outHistoName+".pdf")
+    canvas.SaveAs("TimeMean_vs_xy_"+info.outHistoName+".gif")
+    canvas.SaveAs("TimeMean_vs_xy_"+info.outHistoName+".pdf")
     info.th2.Write()
 
 outputfile.Close()
