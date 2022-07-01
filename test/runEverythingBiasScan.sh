5f7d89f1a69ab26bffc4e7062542ce07c7d5c756
Claudio San Martin <claudio.sanmartinval@gmail.com>
2022-07-01 13:49:16 -0500

    Remove not centered sensor in BiasScan

--- test/runEverythingBiasScan.sh
+++ test/runEverythingBiasScan.sh
@@ -128,31 +128,31 @@ python PlotEfficiency.py    -D EIC_W1_1cm_500up_200uw_245V -x 2.5 -r 1
 python PlotEfficiency.py    -D EIC_W1_1cm_500up_200uw_245V -x 2.5 -r 2
 python plot1DRes.py         -D EIC_W1_1cm_500up_200uw_245V
 
-## EIC_W1_1cm_500up_200uw_250V
-echo "Running over EIC_W1_1cm_500up_200uw_250V sensor"
-cd ../test
-./MyAnalysis -A InitialAnalyzer -D EIC_W1_1cm_500up_200uw_250V
-cd ../macros
-python FindDelayCorrections.py -D EIC_W1_1cm_500up_200uw_250V
-python FindInputHistos4YReco.py -D EIC_W1_1cm_500up_200uw_250V -I
-cd ../test
-./MyAnalysis -A Analyze -D EIC_W1_1cm_500up_200uw_250V
-cd ../macros
+# ## EIC_W1_1cm_500up_200uw_250V
+# echo "Running over EIC_W1_1cm_500up_200uw_250V sensor"
+# cd ../test
+# ./MyAnalysis -A InitialAnalyzer -D EIC_W1_1cm_500up_200uw_250V
+# cd ../macros
+# python FindDelayCorrections.py -D EIC_W1_1cm_500up_200uw_250V
+# python FindInputHistos4YReco.py -D EIC_W1_1cm_500up_200uw_250V -I
+# cd ../test
+# ./MyAnalysis -A Analyze -D EIC_W1_1cm_500up_200uw_250V
+# cd ../macros
 
-python DoPositionRecoFit.py -D EIC_W1_1cm_500up_200uw_250V -A --xmax 0.81 --fitOrder 5
-python PlotAmplitudeVsX.py  -D EIC_W1_1cm_500up_200uw_250V --xlength 2.5 --ylength 90.0
-python PlotAmplitudeVsXY.py -D EIC_W1_1cm_500up_200uw_250V --zmin 20.0 --zmax 90.0
-python PlotTimeDiffVsX.py   -D EIC_W1_1cm_500up_200uw_250V --xlength 2.5 --ylength 150.0
-python PlotTimeDiffVsXY.py  -D EIC_W1_1cm_500up_200uw_250V --zmin 25.0 --zmax 75.0
-python PlotTimeDiffVsY.py   -D EIC_W1_1cm_500up_200uw_250V --xlength 5.2 --ylength 150.0
-python PlotTimeMeanVsXY.py  -D EIC_W1_1cm_500up_200uw_250V --zmin -0.5 --zmax 0.5
-python PlotSimpleXYMaps.py  -D EIC_W1_1cm_500up_200uw_250V
-python PlotRecoDiffVsXY.py  -D EIC_W1_1cm_500up_200uw_250V --zmin 0.0 --zmax 100.0
-python PlotRecoDiffVsX.py   -D EIC_W1_1cm_500up_200uw_250V --xlength 2.5 --ylength 150.0
-python PlotEfficiency.py    -D EIC_W1_1cm_500up_200uw_250V -x 2.5 -r 0
-python PlotEfficiency.py    -D EIC_W1_1cm_500up_200uw_250V -x 2.5 -r 1
-python PlotEfficiency.py    -D EIC_W1_1cm_500up_200uw_250V -x 2.5 -r 2
-python plot1DRes.py         -D EIC_W1_1cm_500up_200uw_250V
+# python DoPositionRecoFit.py -D EIC_W1_1cm_500up_200uw_250V -A --xmax 0.81 --fitOrder 5
+# python PlotAmplitudeVsX.py  -D EIC_W1_1cm_500up_200uw_250V --xlength 2.5 --ylength 90.0
+# python PlotAmplitudeVsXY.py -D EIC_W1_1cm_500up_200uw_250V --zmin 20.0 --zmax 90.0
+# python PlotTimeDiffVsX.py   -D EIC_W1_1cm_500up_200uw_250V --xlength 2.5 --ylength 150.0
+# python PlotTimeDiffVsXY.py  -D EIC_W1_1cm_500up_200uw_250V --zmin 25.0 --zmax 75.0
+# python PlotTimeDiffVsY.py   -D EIC_W1_1cm_500up_200uw_250V --xlength 5.2 --ylength 150.0
+# python PlotTimeMeanVsXY.py  -D EIC_W1_1cm_500up_200uw_250V --zmin -0.5 --zmax 0.5
+# python PlotSimpleXYMaps.py  -D EIC_W1_1cm_500up_200uw_250V
+# python PlotRecoDiffVsXY.py  -D EIC_W1_1cm_500up_200uw_250V --zmin 0.0 --zmax 100.0
+# python PlotRecoDiffVsX.py   -D EIC_W1_1cm_500up_200uw_250V --xlength 2.5 --ylength 150.0
+# python PlotEfficiency.py    -D EIC_W1_1cm_500up_200uw_250V -x 2.5 -r 0
+# python PlotEfficiency.py    -D EIC_W1_1cm_500up_200uw_250V -x 2.5 -r 1
+# python PlotEfficiency.py    -D EIC_W1_1cm_500up_200uw_250V -x 2.5 -r 2
+# python plot1DRes.py         -D EIC_W1_1cm_500up_200uw_250V
 
 ## EIC_W1_1cm_500up_200uw_265V
 echo "Running over EIC_W1_1cm_500up_200uw_265V sensor"
