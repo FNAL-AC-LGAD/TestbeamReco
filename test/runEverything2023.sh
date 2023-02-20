50eac49ca9a676f9f4b3b26a1608b0f270460119
Claudio San Martin <claudio.sanmartinval@gmail.com>
2023-02-20 13:39:05 -0600

    Add sensor to runEverything and add macro to get risetime, charge,... maps

--- test/runEverything2023.sh
+++ test/runEverything2023.sh
@@ -17,7 +17,8 @@ python PlotTimeDiffVsX.py           -D BNL_50um_1cm_450um_W3051_2_2_170V --xleng
 python PlotTimeDiffVsXY.py          -D BNL_50um_1cm_450um_W3051_2_2_170V --zmin 25.0 --zmax 75.0
 python PlotTimeDiffVsY.py           -D BNL_50um_1cm_450um_W3051_2_2_170V --xlength 6.0 --ylength 150.0
 python PlotTimeMeanVsXY.py          -D BNL_50um_1cm_450um_W3051_2_2_170V --zmin -0.5 --zmax 0.5
-python PlotSimpleXYMaps.py          -D BNL_50um_1cm_450um_W3051_2_2_170V
+# python PlotSimpleXYMaps.py          -D BNL_50um_1cm_450um_W3051_2_2_170V
+python PlotAmpChargeVsXY.py         -D BNL_50um_1cm_450um_W3051_2_2_170V
 python PlotRecoDiffVsXY.py          -D BNL_50um_1cm_450um_W3051_2_2_170V --zmin 0.0 --zmax 100.0
 python PlotRecoDiffVsX.py           -D BNL_50um_1cm_450um_W3051_2_2_170V --xlength 2.5 --ylength 150.0
 python PlotEfficiency.py            -D BNL_50um_1cm_450um_W3051_2_2_170V -x 2.5
@@ -227,41 +228,47 @@ python Paper_PlotTimeDiffVsX.py     -D BNL_50um_1cm_450um_W3051_2_2_170V -x 2.5 
 
 
 
-# ## BNL_20um_1cm_450um_W3075_2_4_80V
-# echo "Running over BNL_20um_1cm_450um_W3075_2_4_80V sensor"
-# cd ../test
-# ./MyAnalysis -A InitialAnalyzer -D BNL_20um_1cm_450um_W3075_2_4_80V
-# cd ../macros
-# python FindDelayCorrections.py -D BNL_20um_1cm_450um_W3075_2_4_80V
-# python FindInputHistos4YReco.py -D BNL_20um_1cm_450um_W3075_2_4_80V -I
-# cd ../test
-# ./MyAnalysis -A Analyze -D BNL_20um_1cm_450um_W3075_2_4_80V
-# cd ../macros
+## BNL_20um_1cm_450um_W3075_2_4_80V
+echo "Running over BNL_20um_1cm_450um_W3075_2_4_80V sensor"
+cd ../test
+./MyAnalysis -A InitialAnalyzer -D BNL_20um_1cm_450um_W3075_2_4_80V
+cd ../macros
+python FindDelayCorrections.py -D BNL_20um_1cm_450um_W3075_2_4_80V
+python FindInputHistos4YReco.py -D BNL_20um_1cm_450um_W3075_2_4_80V -I
+cd ../test
+./MyAnalysis -A Analyze -D BNL_20um_1cm_450um_W3075_2_4_80V
+cd ../macros
 
-# python DoPositionRecoFit.py         -D BNL_20um_1cm_450um_W3075_2_4_80V -A --xmax 0.79 --fitOrder 5
-# python PlotAmplitudeVsX.py          -D BNL_20um_1cm_450um_W3075_2_4_80V --xlength 2.5 --ylength 100.0
-# python PlotAmplitudeVsXY.py         -D BNL_20um_1cm_450um_W3075_2_4_80V --zmin 10.0 --zmax 50.0
-# python PlotTimeDiffVsX.py           -D BNL_20um_1cm_450um_W3075_2_4_80V --xlength 2.5 --ylength 150.0
-# python PlotTimeDiffVsXY.py          -D BNL_20um_1cm_450um_W3075_2_4_80V --zmin 25.0 --zmax 75.0
-# python PlotTimeDiffVsY.py           -D BNL_20um_1cm_450um_W3075_2_4_80V --xlength 6.0 --ylength 150.0
-# python PlotTimeMeanVsXY.py          -D BNL_20um_1cm_450um_W3075_2_4_80V --zmin -0.5 --zmax 0.5
+python DoPositionRecoFit.py         -D BNL_20um_1cm_450um_W3075_2_4_80V -A --xmax 0.75 --fitOrder 5
+python PlotAmplitudeVsX.py          -D BNL_20um_1cm_450um_W3075_2_4_80V --xlength 2.5 --ylength 100.0
+python PlotAmplitudeVsXY.py         -D BNL_20um_1cm_450um_W3075_2_4_80V --zmin 10.0 --zmax 40.0
+python PlotTimeDiffVsX.py           -D BNL_20um_1cm_450um_W3075_2_4_80V --xlength 2.5 --ylength 150.0
+python PlotTimeDiffVsXY.py          -D BNL_20um_1cm_450um_W3075_2_4_80V --zmin 25.0 --zmax 75.0
+python PlotTimeDiffVsY.py           -D BNL_20um_1cm_450um_W3075_2_4_80V --xlength 6.0 --ylength 150.0
+python PlotTimeMeanVsXY.py          -D BNL_20um_1cm_450um_W3075_2_4_80V --zmin -0.5 --zmax 0.5
 # python PlotSimpleXYMaps.py          -D BNL_20um_1cm_450um_W3075_2_4_80V
-# python PlotRecoDiffVsXY.py          -D BNL_20um_1cm_450um_W3075_2_4_80V --zmin 0.0 --zmax 100.0
-# python PlotRecoDiffVsX.py           -D BNL_20um_1cm_450um_W3075_2_4_80V --xlength 2.5 --ylength 150.0
-# python PlotEfficiency.py            -D BNL_20um_1cm_450um_W3075_2_4_80V -x 2.5
-# python plot1DRes.py                 -D BNL_20um_1cm_450um_W3075_2_4_80V
-# python PlotRecoDiffVsY.py           -D BNL_20um_1cm_450um_W3075_2_4_80V --xlength 5.6 --ylength 4.0
-# python PlotCutFlow.py               -D BNL_20um_1cm_450um_W3075_2_4_80V
+python PlotAmpChargeVsXY.py         -D BNL_20um_1cm_450um_W3075_2_4_80V
+python PlotRecoDiffVsXY.py          -D BNL_20um_1cm_450um_W3075_2_4_80V --zmin 0.0 --zmax 100.0
+python PlotRecoDiffVsX.py           -D BNL_20um_1cm_450um_W3075_2_4_80V --xlength 2.5 --ylength 150.0
+python PlotEfficiency.py            -D BNL_20um_1cm_450um_W3075_2_4_80V -x 2.5
+python plot1DRes.py                 -D BNL_20um_1cm_450um_W3075_2_4_80V
+python PlotRecoDiffVsY.py           -D BNL_20um_1cm_450um_W3075_2_4_80V --xlength 5.6 --ylength 4.0
+python PlotCutFlow.py               -D BNL_20um_1cm_450um_W3075_2_4_80V
 
-# # Paper plots
-# # python Paper_1DRes.py               -D BNL_20um_1cm_450um_W3075_2_4_80V
-# # python Paper_Efficiency.py          -D BNL_20um_1cm_450um_W3075_2_4_80V -x 1.2
-# # python Paper_XRes.py                -D BNL_20um_1cm_450um_W3075_2_4_80V -x 1.2
-# # python Paper_PlotTimeDiffVsX.py     -D BNL_20um_1cm_450um_W3075_2_4_80V -x 1.2 -y 82
-# # python Paper_XRes.py                -D BNL_20um_1cm_450um_W3075_2_4_80V -x 1.2 --hot
-# # python Paper_PlotTimeDiffVsX.py     -D BNL_20um_1cm_450um_W3075_2_4_80V -x 1.2 -y 82 --hot
-# # python Paper_Summary_XRes_Time.py   -D BNL_20um_1cm_450um_W3075_2_4_80V -x 1.2 -y 70 --hot
+# Paper plots
+python Paper_1DRes.py               -D BNL_20um_1cm_450um_W3075_2_4_80V
+python Paper_Efficiency.py          -D BNL_20um_1cm_450um_W3075_2_4_80V -x 2.5
+python Paper_XRes.py                -D BNL_20um_1cm_450um_W3075_2_4_80V -x 2.5
+python Paper_PlotTimeDiffVsX.py     -D BNL_20um_1cm_450um_W3075_2_4_80V -x 2.5 -y 150
 
+python Paper_1DRes.py               -D BNL_20um_1cm_450um_W3075_2_4_80V -t
+python Paper_Efficiency.py          -D BNL_20um_1cm_450um_W3075_2_4_80V -x 2.5 -t
+python Paper_XRes.py                -D BNL_20um_1cm_450um_W3075_2_4_80V -x 2.5 -t
+python Paper_PlotTimeDiffVsX.py     -D BNL_20um_1cm_450um_W3075_2_4_80V -x 2.5 -y 150 -t
+
+# python Paper_XRes.py                -D BNL_20um_1cm_450um_W3075_2_4_80V -x 1.2 --hot
+# python Paper_PlotTimeDiffVsX.py     -D BNL_20um_1cm_450um_W3075_2_4_80V -x 1.2 -y 82 --hot
+# python Paper_Summary_XRes_Time.py   -D BNL_20um_1cm_450um_W3075_2_4_80V -x 1.2 -y 70 --hot
 
 
 ## BNL_50um_2p5cm_mixConfig1_W3051_1_4_170V
@@ -282,7 +289,8 @@ python PlotTimeDiffVsX.py           -D BNL_50um_2p5cm_mixConfig1_W3051_1_4_170V 
 python PlotTimeDiffVsXY.py          -D BNL_50um_2p5cm_mixConfig1_W3051_1_4_170V --zmin 25.0 --zmax 75.0
 python PlotTimeDiffVsY.py           -D BNL_50um_2p5cm_mixConfig1_W3051_1_4_170V --xlength 13.5 --ylength 150.0
 python PlotTimeMeanVsXY.py          -D BNL_50um_2p5cm_mixConfig1_W3051_1_4_170V --zmin -0.5 --zmax 0.5
-python PlotSimpleXYMaps.py          -D BNL_50um_2p5cm_mixConfig1_W3051_1_4_170V
+# python PlotSimpleXYMaps.py          -D BNL_50um_2p5cm_mixConfig1_W3051_1_4_170V
+python PlotAmpChargeVsXY.py         -D BNL_50um_2p5cm_mixConfig1_W3051_1_4_170V
 python PlotRecoDiffVsXY.py          -D BNL_50um_2p5cm_mixConfig1_W3051_1_4_170V --zmin 0.0 --zmax 100.0
 python PlotRecoDiffVsX.py           -D BNL_50um_2p5cm_mixConfig1_W3051_1_4_170V --xlength 2.7 --ylength 150.0
 python PlotEfficiency.py            -D BNL_50um_2p5cm_mixConfig1_W3051_1_4_170V -x 2.7
@@ -324,7 +332,8 @@ python PlotTimeDiffVsX.py           -D BNL_50um_2p5cm_mixConfig2_W3051_1_4_170V 
 python PlotTimeDiffVsXY.py          -D BNL_50um_2p5cm_mixConfig2_W3051_1_4_170V --zmin 25.0 --zmax 75.0
 python PlotTimeDiffVsY.py           -D BNL_50um_2p5cm_mixConfig2_W3051_1_4_170V --xlength 13.5 --ylength 150.0
 python PlotTimeMeanVsXY.py          -D BNL_50um_2p5cm_mixConfig2_W3051_1_4_170V --zmin -0.5 --zmax 0.5
-python PlotSimpleXYMaps.py          -D BNL_50um_2p5cm_mixConfig2_W3051_1_4_170V
+# python PlotSimpleXYMaps.py          -D BNL_50um_2p5cm_mixConfig2_W3051_1_4_170V
+python PlotAmpChargeVsXY.py         -D BNL_50um_2p5cm_mixConfig2_W3051_1_4_170V
 python PlotRecoDiffVsXY.py          -D BNL_50um_2p5cm_mixConfig2_W3051_1_4_170V --zmin 0.0 --zmax 100.0
 python PlotRecoDiffVsX.py           -D BNL_50um_2p5cm_mixConfig2_W3051_1_4_170V --xlength 2.7 --ylength 150.0
 python PlotEfficiency.py            -D BNL_50um_2p5cm_mixConfig2_W3051_1_4_170V -x 2.7
