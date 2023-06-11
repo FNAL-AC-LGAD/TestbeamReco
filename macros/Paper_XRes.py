a3e98aad4a9c9bec9057271c433c0aea0350d084
Claudio San Martin <claudio.sanmartinval@gmail.com>
2023-06-11 15:35:28 -0500

    Small improvement in code

--- macros/Paper_XRes.py
+++ macros/Paper_XRes.py
@@ -178,11 +178,10 @@ if (is_hotspot):
     hist_twoStrip_fullSnsr = input_fullSnsr.Get("h_twoStrip")
 
 # oneStripResValue = myStyle.resolutions2022[dataset]['position_oneStripRMS']
-try:
-    oneStripResValue_list = myStyle.resolutions2022OneStripChannel[dataset]['resOneStrip']
-except:
-    oneStripResValue_list = myStyle.resolutions2023OneStripChannel[dataset]['resOneStrip']
-
+l_resolution_dicts = [myStyle.resolutions2022OneStripChannel, myStyle.resolutions2023OneStripChannel]
+for r_dict in l_resolution_dicts:
+    if dataset in r_dict:
+        oneStripResValue_list = r_dict[dataset]['resOneStrip']
 
 max_strip_edge = hist_info_twoStrip.f.Get("stripBoxInfo01").GetMean(1) + strip_width/2000.
 if useShift: max_strip_edge -= hist_info_twoStrip.shift()
