3440b2eb7a19548aa478f995b25e8732b26de063
Claudio San Martin <claudio.sanmartinval@gmail.com>
2023-11-24 13:02:03 -0600

    Fix format issues

--- macros/CompareSetups_Amp.py
+++ macros/CompareSetups_Amp.py
@@ -32,44 +32,48 @@ os.makedirs("../output/compare/", exist_ok=True)
 
 
 
-sensors_list = [ # Varying resistivity and capacitance
-                ["HPK_W4_17_2_50T_1P0_500P_50M_C240_204V", "HPK_W2_3_2_50T_1P0_500P_50M_E240_180V", "HPK_W8_17_2_50T_1P0_500P_50M_C600_200V", "HPK_W5_17_2_50T_1P0_500P_50M_E600_190V"],
-                #HPK Varying thickness
-                ["HPK_W5_17_2_50T_1P0_500P_50M_E600_190V", "HPK_W9_15_2_20T_1P0_500P_50M_E600_114V"],
-                # KOJI Varying thickness
-                ["HPK_KOJI_50T_1P0_80P_60M_E240_190V", "HPK_KOJI_20T_1P0_80P_60M_E240_112V"],
-                # BNL and HPK Varying metal widths
-                ["BNL_50um_1cm_400um_W3051_1_4_160V", "BNL_50um_1cm_450um_W3051_2_2_170V", "HPK_W8_17_2_50T_1P0_500P_50M_C600_200V", "HPK_W8_18_2_50T_1P0_500P_100M_C600_208V"]
-        ]
-tagVar_list = [ # Varying resistivity and capacitance
-             ["resistivityNumber", "capacitance"],
-                #HPK Varying thickness
-            ["thickness"],
-                # KOJI Varying thickness
-            ["thickness"],
-                # BNL and HPK Varying metal widths
-            ["width"]
-        ]
+sensors_list = [
+    # Varying resistivity and capacitance
+    ["HPK_W4_17_2_50T_1P0_500P_50M_C240_204V", "HPK_W2_3_2_50T_1P0_500P_50M_E240_180V", "HPK_W8_17_2_50T_1P0_500P_50M_C600_200V", "HPK_W5_17_2_50T_1P0_500P_50M_E600_190V"],
+    # HPK Varying thickness
+    ["HPK_W5_17_2_50T_1P0_500P_50M_E600_190V", "HPK_W9_15_2_20T_1P0_500P_50M_E600_114V"],
+    # KOJI Varying thickness
+    ["HPK_KOJI_50T_1P0_80P_60M_E240_190V", "HPK_KOJI_20T_1P0_80P_60M_E240_112V"],
+    # BNL and HPK Varying metal widths
+    # ["BNL_50um_1cm_400um_W3051_1_4_160V", "BNL_50um_1cm_450um_W3051_2_2_170V", "HPK_W8_17_2_50T_1P0_500P_50M_C600_200V", "HPK_W8_18_2_50T_1P0_500P_100M_C600_208V"],
+]
+tagVar_list = [
+    # Varying resistivity and capacitance
+    ["resistivityNumber", "capacitance"],
+    # HPK Varying thickness
+    ["thickness"],
+    # KOJI Varying thickness
+    ["thickness"],
+    # BNL and HPK Varying metal widths
+    # ["width"],
+]
 
-ylength_list = [ # Varying resistivity and capacitance 
-                190,
-                #HPK Varying thickness
-                170,
-                # KOJI Varying thickness
-                100,
-                # BNL and HPK Varying metal widths
-                100
-        ]
+ylength_list = [
+    # Varying resistivity and capacitance
+    190,
+    # HPK Varying thickness
+    170,
+    # KOJI Varying thickness
+    100,
+    # BNL and HPK Varying metal widths
+    # 100,
+]
 
-saveName_list = [ # Varying resistivity and capacitance 
-                "../output/compare/HPK_Amplitude_vs_x_ResCap",
-                #HPK Varying thickness
-                "../output/compare/HPK_Amplitude_vs_x_thickness",
-                # KOJI Varying thickness
-                "../output/compare/Koji_Amplitude_vs_x_thicknessg",
-                # BNL and HPK Varying metal widths
-                "../output/compare/BNL_and_HPK_Amplitude_vs_x_MetalWidth"
-        ]
+saveName_list = [
+    # Varying resistivity and capacitance
+    "../output/compare/HPK_Amplitude_vs_x_ResCap",
+    # HPK Varying thickness
+    "../output/compare/HPK_Amplitude_vs_x_thickness",
+    # KOJI Varying thickness
+    "../output/compare/Koji_Amplitude_vs_x_thicknessg",
+    # BNL and HPK Varying metal widths
+    # "../output/compare/BNL_and_HPK_Amplitude_vs_x_MetalWidth",
+]
 
 #Make final plots
 # Varying resistivity and capacitance
@@ -107,7 +111,6 @@ ymin = 1
 
 for sensors, tagVars, ylength, saveName in zip(sensors_list, tagVar_list, ylength_list, saveName_list):
 
-    
     yLegend = 0.026*len(sensors)
     legend2 = TLegend(2*myStyle.GetMargin()+0.065,1-myStyle.GetMargin()-0.2-yLegend,1-myStyle.GetMargin()-0.065,1-myStyle.GetMargin()-0.03)
     legend2.SetBorderSize(1)
@@ -117,14 +120,14 @@ for sensors, tagVars, ylength, saveName in zip(sensors_list, tagVar_list, ylengt
 
     sensor_prod="BNL & HPK Production"
     if ("BNL" in sensors[0]):
-       sensor_prod = "BNL & HPK Production"
+        sensor_prod = "BNL & HPK Production"
     else:
-       sensor_prod = "HPK Production"
+        sensor_prod = "HPK Production"
     if ("KOJI" in sensors[0]):
         xlength = 0.25
 
     tag = get_legend_comparation_plots(sensors, tagVars)
-     
+
     totalAmplitude_vs_x = TH1F("htemp","",1,-xlength,xlength)
     totalAmplitude_vs_x.Draw("AXIS")
     totalAmplitude_vs_x.SetStats(0)
@@ -139,20 +142,20 @@ for sensors, tagVars, ylength, saveName in zip(sensors_list, tagVar_list, ylengt
     shift = inputfile.Get("stripBoxInfo03").GetMean(1)
     boxes = getStripBox(inputfile,ymin,ylength- 30,False, 18, True, shift)
     for box in boxes[1:len(boxes)-1]:
-       box.Draw()
+        box.Draw()
 
 # Draw dotted line for 100 micron strip width
     if(any("100M" in iter for iter in sensors)):
         if(any("50M" in iter for iter in sensors)):
             for i in range(1,6):
-                vertical_line = TLine((i-3)*0.5-0.05, 0, (i-3)*0.5-0.05, ylength-10) 
+                vertical_line = TLine((i-3)*0.5-0.05, 0, (i-3)*0.5-0.05, ylength-10)
                 vertical_line.SetLineWidth(2)
                 vertical_line.SetLineColor(14)
                 vertical_line.SetLineColorAlpha(14,0.4)
                 vertical_line.SetLineStyle(9)
                 vertical_line.DrawClone("same")
 
-                vertical_line2 = TLine((i-3)*0.5+0.05, 0, (i-3)*0.5+0.05, ylength-10) 
+                vertical_line2 = TLine((i-3)*0.5+0.05, 0, (i-3)*0.5+0.05, ylength-10)
                 vertical_line2.SetLineWidth(2)
                 vertical_line2.SetLineColor(14)
                 vertical_line2.SetLineColorAlpha(14,0.4)
@@ -173,9 +176,6 @@ for sensors, tagVars, ylength, saveName in zip(sensors_list, tagVar_list, ylengt
         lengendEntry = legend2.AddEntry(plotList_amplitude_vs_x[i], tag[i])
         # lengendEntry.SetTextAlign(22)
 
-# legend2.AddEntry(plotList_amplitude_vs_x, "")
-
-
     legend2.Draw()
     myStyle.BeamInfo()
     myStyle.SensorProductionInfo(sensor_prod)
@@ -187,12 +187,12 @@ for sensors, tagVars, ylength, saveName in zip(sensors_list, tagVar_list, ylengt
     # legendHeader = "#bf{%s}"%legendHeader
     legendHeader = tag[-1]
     legend2.SetHeader(legendHeader, "C")
-# myStyle.SensorInfoSmart(dataset)
+    # myStyle.SensorInfoSmart(dataset)
 
-# canvas.SaveAs("../HPK_Amplitude_vs_x_ResCap.png")
-# canvas.SaveAs("../HPK_Amplitude_vs_x_thickness.png")
-# canvas.SaveAs("../Koji_Amplitude_vs_x_thickness.png")
-# canvas.SaveAs("../BNL_and_HPK_Amplitude_vs_x_MetalWidth.png")
+    # canvas.SaveAs("../HPK_Amplitude_vs_x_ResCap.png")
+    # canvas.SaveAs("../HPK_Amplitude_vs_x_thickness.png")
+    # canvas.SaveAs("../Koji_Amplitude_vs_x_thickness.png")
+    # canvas.SaveAs("../BNL_and_HPK_Amplitude_vs_x_MetalWidth.png")
     canvas.SaveAs(saveName + ".png")
     canvas.SaveAs(saveName + ".pdf")
     for i in range(len(plotfile)):
