a65f28e7ec661c1c538c113acb00f5ebbc7588c2
Danush Shekar <67160286+CodexForster@users.noreply.github.com>
2024-01-25 15:13:08 -0600

    Update Analyze.cc

--- src/Analyze.cc
+++ src/Analyze.cc
@@ -1038,7 +1038,7 @@ void Analyze::Loop(NTupleReader& tr, int maxevents)
                 // Hit on metal strip
                 double metalLeft = stripCenterXPositionLGAD[rowIndex][i] - stripWidth/2.;
                 double metalRight = stripCenterXPositionLGAD[rowIndex][i] + stripWidth/2.;
-                if (isHPKStrips && stripCenterXPositionLGAD[rowIndex][i]!=0.0 && (metalLeft < x) && (x < metalRight))
+                if (!isPadSensor && stripCenterXPositionLGAD[rowIndex][i]!=0.0 && (metalLeft < x) && (x < metalRight))
                 {
                     hitOnMetal = true;
                 }
