aa7c492312c4e3248b2c4dae620a741fd8fb475a
Danush Shekar <dsheka3@uic.edu>
2024-03-09 00:22:59 -0600

    analyze restored?!

--- src/Analyze.cc
+++ src/Analyze.cc
@@ -8,6 +8,7 @@
 #include <TEfficiency.h>
 #include <TFile.h>
 #include <iostream>
+#include <fstream>
 
 Analyze::Analyze()
 {
@@ -340,7 +341,7 @@ void Analyze::InitHistos(NTupleReader& tr, const std::vector<std::vector<int>>& 
     utility::makeHisto(my_histos,"slewrate_NoSum","", 300,0.0,400.0);
     utility::makeHisto(my_histos,"slewRateChargeRatio_NoSum","", 300,0.0,50.0);
     utility::makeHisto(my_histos,"weighted2_jitter_NoSum","", 200,0.0,100.0);
-    utility::makeHisto(my_histos,"weighted2_jitter_NewDef_NoSum","", 200,0.0,100.0);
+    utility::makeHisto(my_histos,"weighted2_jitterOldDef_NoSum","", 200,0.0,100.0);
 
     // Overall
     utility::makeHisto(my_histos,"ampMax_Overall", "", 450, -50.0, 400.0);
@@ -351,7 +352,7 @@ void Analyze::InitHistos(NTupleReader& tr, const std::vector<std::vector<int>>& 
     utility::makeHisto(my_histos,"slewrate_Overall","", 300,0.0,400.0);
     utility::makeHisto(my_histos,"slewRateChargeRatio_Overall","", 300,0.0,50.0);
     utility::makeHisto(my_histos,"weighted2_jitter_Overall","", 200,0.0,100.0);
-    utility::makeHisto(my_histos,"weighted2_jitter_NewDef_Overall","", 200,0.0,100.0);
+    utility::makeHisto(my_histos,"weighted2_jitterOldDef_Overall","", 200,0.0,100.0);
 
     // Metal
     utility::makeHisto(my_histos,"ampMax_Metal", "", 450, -50.0, 400.0);
@@ -362,7 +363,7 @@ void Analyze::InitHistos(NTupleReader& tr, const std::vector<std::vector<int>>& 
     utility::makeHisto(my_histos,"slewrate_Metal","", 300,0.0,400.0);
     utility::makeHisto(my_histos,"slewRateChargeRatio_Metal","", 300,0.0,50.0);
     utility::makeHisto(my_histos,"weighted2_jitter_Metal","", 200,0.0,100.0);
-    utility::makeHisto(my_histos,"weighted2_jitter_NewDef_Metal","", 200,0.0,100.0);
+    utility::makeHisto(my_histos,"weighted2_jitterOldDef_Metal","", 200,0.0,100.0);
 
     // Gap
     utility::makeHisto(my_histos,"ampMax_Gap", "", 450, -50.0, 400.0);
@@ -373,7 +374,7 @@ void Analyze::InitHistos(NTupleReader& tr, const std::vector<std::vector<int>>& 
     utility::makeHisto(my_histos,"slewrate_Gap","", 300,0.0,400.0);
     utility::makeHisto(my_histos,"slewRateChargeRatio_Gap","", 300,0.0,50.0);
     utility::makeHisto(my_histos,"weighted2_jitter_Gap","", 200,0.0,100.0);
-    utility::makeHisto(my_histos,"weighted2_jitter_NewDef_Gap","", 200,0.0,100.0);
+    utility::makeHisto(my_histos,"weighted2_jitterOldDef_Gap","", 200,0.0,100.0);
 
     // Middle gap
     utility::makeHisto(my_histos,"ampMax_MidGap", "", 450, -50.0, 400.0);
@@ -384,7 +385,7 @@ void Analyze::InitHistos(NTupleReader& tr, const std::vector<std::vector<int>>& 
     utility::makeHisto(my_histos,"slewrate_MidGap","", 300,0.0,400.0);
     utility::makeHisto(my_histos,"slewRateChargeRatio_MidGap","", 300,0.0,50.0);
     utility::makeHisto(my_histos,"weighted2_jitter_MidGap","", 200,0.0,100.0);
-    utility::makeHisto(my_histos,"weighted2_jitter_NewDef_MidGap","", 200,0.0,100.0);
+    utility::makeHisto(my_histos,"weighted2_jitterOldDef_MidGap","", 200,0.0,100.0);
 
     // Region of interest (ROI)
     for(unsigned int k = 0; k < regionsOfIntrest.size(); k++)
@@ -596,7 +597,9 @@ void Analyze::InitHistos(NTupleReader& tr, const std::vector<std::vector<int>>& 
     utility::makeHisto(my_3d_histos,"ampChargeRatio_vs_xy","; X [mm]; Y [mm]",std::round((xmax-xmin)/xBinSize),xmin,xmax, std::round((ymax-ymin)/yBinSize),ymin,ymax,300,0.0,50.0);
     utility::makeHisto(my_3d_histos,"slewRate_vs_xy","; X [mm]; Y [mm]",std::round((xmax-xmin)/xBinSize),xmin,xmax, std::round((ymax-ymin)/yBinSize),ymin,ymax,300,0.0,500.0);
     utility::makeHisto(my_3d_histos,"weighted2_jitter_vs_xy", "; X [mm]; Y [mm]", std::round((xmax-xmin)/xBinSize),xmin,xmax, std::round((ymax-ymin)/yBinSize),ymin,ymax, 200.0, 0.0, 200.0);
+    utility::makeHisto(my_3d_histos,"weighted2_jitterOldDef_vs_xy", "; X [mm]; Y [mm]", std::round((xmax-xmin)/xBinSize),xmin,xmax, std::round((ymax-ymin)/yBinSize),ymin,ymax, 200.0, 0.0, 200.0);
     utility::makeHisto(my_3d_histos,"weighted2_jitter_vs_xy_tight", "; X [mm]; Y [mm]", std::round((xmax-xmin)/xBinSize),xmin,xmax, std::round((ymax-ymin)/yBinSize),ymin,ymax, 200.0, 0.0, 200.0);
+    utility::makeHisto(my_3d_histos,"weighted2_jitterOldDef_vs_xy_tight", "; X [mm]; Y [mm]", std::round((xmax-xmin)/xBinSize),xmin,xmax, std::round((ymax-ymin)/yBinSize),ymin,ymax, 200.0, 0.0, 200.0);
 
     utility::makeHisto(my_3d_histos,"timeDiff_vs_xy_tight", "; X [mm]; Y [mm]", std::round((xmax-xmin)/xBinSize),xmin,xmax, timeDiffYnbin,ymin,ymax, timeDiffNbin,timeDiffLow,timeDiffHigh);
     utility::makeHisto(my_3d_histos,"timeDiffTracker_vs_xy_tight", "; X [mm]; Y [mm]", std::round((xmax-xmin)/xBinSize),xmin,xmax, timeDiffYnbin,ymin,ymax, timeDiffNbin,timeDiffLow,timeDiffHigh);
@@ -762,6 +765,9 @@ void Analyze::Loop(NTupleReader& tr, int maxevents)
     int lowGoodStrip = indexToGeometryMap.at(lowGoodStripIndex)[1];
     int highGoodStrip = indexToGeometryMap.at(highGoodStripIndex)[1];
     bool plotWaveForm = false;
+
+    int counter[2] = {0, 0};
+    int max_save = 20;
 
     if(firstFile) InitHistos(tr, geometry);
 
@@ -1137,7 +1143,7 @@ void Analyze::Loop(NTupleReader& tr, int maxevents)
                 utility::fillHisto(passChannel && goodHit && goodNearHit,                          my_2d_histos, "Amp1OverAmp1and2_vs_x_channel"+r+s+"_NearHit", x,Amp1OverAmp1and2);
                 */
                 utility::fillHisto(passChannel && goodHit,                                         my_2d_histos, "relFrac_vs_y_channel"+r+s, y,relFracChannel);
-                utility::fillHisto(passChannel && goodHit && isMaxChannel && goodNeighbour,        my_2d_histos, "Amp1OverAmp1and2_vs_deltaXmax_channel"+r+s, deltaXmax, Amp1OverAmp1and2);
+                utility::fillHisto(passChannel && goodHit && isMaxChannel && goodNeighbour,        my_2d_histos, "Amp1OverAmp1and2_vs_deltaXmax_channel"+r+s, fabs(deltaXmax), Amp1OverAmp1and2);
 
                 utility::fillHisto(pass_tightY && goodHit && goodNearHit,                          my_2d_histos, "AmpOverMaxAmp_vs_x_channel"+r+s, x-stripXPosition,fracMaxChannel);
 
@@ -1363,8 +1369,8 @@ void Analyze::Loop(NTupleReader& tr, int maxevents)
         utility::fillHisto(pass && goodAmpHit,                      my_histos, "baselineRMS_NoSum",baselineMaxChannel);
         utility::fillHisto(pass && goodAmpHit,                      my_histos, "slewrate_NoSum", slewrateMaxChannel);
         utility::fillHisto(pass && goodAmpHit,                      my_histos, "slewRateChargeRatio_NoSum", slewRateChargeRatioMaxChannel);
-        utility::fillHisto(pass && goodAmpHit,                      my_histos, "weighted2_jitter_NoSum", weighted2_jitter);
-        utility::fillHisto(pass && goodAmpHit,                      my_histos, "weighted2_jitter_NewDef_NoSum", weighted2_jitter_NewDef);
+        utility::fillHisto(pass && goodAmpHit,                      my_histos, "weighted2_jitter_NoSum", weighted2_jitter_NewDef);
+        utility::fillHisto(pass && goodAmpHit,                      my_histos, "weighted2_jitterOldDef_NoSum", weighted2_jitter);
 
         // Overall
         utility::fillHisto(pass && goodAmpColHit,                   my_histos, "ampMax_Overall", maxAmp);
@@ -1374,8 +1380,8 @@ void Analyze::Loop(NTupleReader& tr, int maxevents)
         utility::fillHisto(pass && goodAmpColHit,                   my_histos, "baselineRMS_Overall",baselineMaxChannel);
         utility::fillHisto(pass && goodAmpColHit,                   my_histos, "slewrate_Overall", slewrateMaxChannel);
         utility::fillHisto(pass && goodAmpColHit,                   my_histos, "slewRateChargeRatio_Overall", slewRateChargeRatioMaxChannel);
-        utility::fillHisto(pass && goodAmpColHit,                   my_histos, "weighted2_jitter_Overall", weighted2_jitter);
-        utility::fillHisto(pass && goodAmpColHit,                   my_histos, "weighted2_jitter_NewDef_Overall", weighted2_jitter_NewDef);
+        utility::fillHisto(pass && goodAmpColHit,                   my_histos, "weighted2_jitter_Overall", weighted2_jitter_NewDef);
+        utility::fillHisto(pass && goodAmpColHit,                   my_histos, "weighted2_jitterOldDef_Overall", weighted2_jitter);
 
         // Metal
         utility::fillHisto(pass && goodAmpColHit && hitOnMetal,     my_histos, "ampMax_Metal", maxAmp);
@@ -1385,8 +1391,8 @@ void Analyze::Loop(NTupleReader& tr, int maxevents)
         utility::fillHisto(pass && goodAmpColHit && hitOnMetal,     my_histos, "baselineRMS_Metal",baselineMaxChannel);
         utility::fillHisto(pass && goodAmpColHit && hitOnMetal,     my_histos, "slewrate_Metal", slewrateMaxChannel);
         utility::fillHisto(pass && goodAmpColHit && hitOnMetal,     my_histos, "slewRateChargeRatio_Metal", slewRateChargeRatioMaxChannel);
-        utility::fillHisto(pass && goodAmpColHit && hitOnMetal,     my_histos, "weighted2_jitter_Metal", weighted2_jitter);
-        utility::fillHisto(pass && goodAmpColHit && hitOnMetal,     my_histos, "weighted2_jitter_NewDef_Metal", weighted2_jitter_NewDef);
+        utility::fillHisto(pass && goodAmpColHit && hitOnMetal,     my_histos, "weighted2_jitter_Metal", weighted2_jitter_NewDef);
+        utility::fillHisto(pass && goodAmpColHit && hitOnMetal,     my_histos, "weighted2_jitterOldDef_Metal", weighted2_jitter);
 
         // Gap
         utility::fillHisto(pass && goodAmpColHit && !hitOnMetal,    my_histos, "ampMax_Gap", maxAmp);
@@ -1396,8 +1402,8 @@ void Analyze::Loop(NTupleReader& tr, int maxevents)
         utility::fillHisto(pass && goodAmpColHit && !hitOnMetal,    my_histos, "baselineRMS_Gap",baselineMaxChannel);
         utility::fillHisto(pass && goodAmpColHit && !hitOnMetal,    my_histos, "slewrate_Gap", slewrateMaxChannel);
         utility::fillHisto(pass && goodAmpColHit && !hitOnMetal,    my_histos, "slewRateChargeRatio_Gap", slewRateChargeRatioMaxChannel);
-        utility::fillHisto(pass && goodAmpColHit && !hitOnMetal,    my_histos, "weighted2_jitter_Gap", weighted2_jitter);
-        utility::fillHisto(pass && goodAmpColHit && !hitOnMetal,    my_histos, "weighted2_jitter_NewDef_Gap", weighted2_jitter_NewDef);
+        utility::fillHisto(pass && goodAmpColHit && !hitOnMetal,    my_histos, "weighted2_jitter_Gap", weighted2_jitter_NewDef);
+        utility::fillHisto(pass && goodAmpColHit && !hitOnMetal,    my_histos, "weighted2_jitterOldDef_Gap", weighted2_jitter);
 
         // Middle gap
         utility::fillHisto(pass && goodAmpColHit && hitOnMidGap,    my_histos, "ampMax_MidGap", maxAmp);
@@ -1407,8 +1413,8 @@ void Analyze::Loop(NTupleReader& tr, int maxevents)
         utility::fillHisto(pass && goodAmpColHit && hitOnMidGap,    my_histos, "baselineRMS_MidGap",baselineMaxChannel);
         utility::fillHisto(pass && goodAmpColHit && hitOnMidGap,    my_histos, "slewrate_MidGap", slewrateMaxChannel);
         utility::fillHisto(pass && goodAmpColHit && hitOnMidGap,    my_histos, "slewRateChargeRatio_MidGap", slewRateChargeRatioMaxChannel);
-        utility::fillHisto(pass && goodAmpColHit && hitOnMidGap,    my_histos, "weighted2_jitter_MidGap", weighted2_jitter);
-        utility::fillHisto(pass && goodAmpColHit && hitOnMidGap,    my_histos, "weighted2_jitter_NewDef_MidGap", weighted2_jitter_NewDef);
+        utility::fillHisto(pass && goodAmpColHit && hitOnMidGap,    my_histos, "weighted2_jitter_MidGap", weighted2_jitter_NewDef);
+        utility::fillHisto(pass && goodAmpColHit && hitOnMidGap,    my_histos, "weighted2_jitterOldDef_MidGap", weighted2_jitter);
 
 
         // Region of interest (ROI)
@@ -1509,8 +1515,10 @@ void Analyze::Loop(NTupleReader& tr, int maxevents)
         utility::fillHisto(pass && goodMaxLGADAmp,                                  my_3d_histos, "charge_vs_xy", x,y,chargeMaxChannel);
         utility::fillHisto(pass && goodMaxLGADAmp,                                  my_3d_histos, "ampChargeRatio_vs_xy", x,y,ampChargeRatioMaxChannel);
         utility::fillHisto(pass && goodMaxLGADAmp,                                  my_3d_histos, "slewRate_vs_xy", x,y,slewrateMaxChannel);
-        utility::fillHisto(pass && goodAmpColHit,                                   my_3d_histos, "weighted2_jitter_vs_xy", x,y,weighted2_jitter);
-        utility::fillHisto(pass_NoXYEdges && goodAmpColHit,                         my_3d_histos, "weighted2_jitter_vs_xy_tight", x,y,weighted2_jitter);
+        utility::fillHisto(pass && goodAmpColHit,                                   my_3d_histos, "weighted2_jitter_vs_xy", x,y,weighted2_jitter_NewDef);
+        utility::fillHisto(pass && goodAmpColHit,                                   my_3d_histos, "weighted2_jitterOldDef_vs_xy", x,y,weighted2_jitter);
+        utility::fillHisto(pass_NoXYEdges && goodAmpColHit,                         my_3d_histos, "weighted2_jitter_vs_xy_tight", x,y,weighted2_jitter_NewDef);
+        utility::fillHisto(pass_NoXYEdges && goodAmpColHit,                         my_3d_histos, "weighted2_jitterOldDef_vs_xy_tight", x,y,weighted2_jitter);
 
 
         utility::fillHisto(pass && goodMaxLGADAmp,                                  my_3d_histos, "totgoodamplitude_vs_xy", x,y,totGoodAmpLGAD);
@@ -1729,7 +1737,7 @@ void Analyze::Loop(NTupleReader& tr, int maxevents)
         //    std::cout<<abs( corrAmp[2] - corrAmp[4])<<" "<<corrAmp[2]<<" "<<(abs( corrAmp[2] - corrAmp[4]) / corrAmp[2])<<std::endl;
         //}
         //if(plotWaveForm && pass && maxAmpInCenter && goodMaxLGADAmp && maxAmpLGAD > 100.0 && maxAmpLGAD < 120.0 && directHit)
-        if(plotWaveForm && goodMaxLGADAmp && maxAmpInCenter)
+        if(plotWaveForm && goodMaxLGADAmp)
         {
             const auto& channel = tr.getVecVec<float>("channel");
             const auto& time = tr.getVecVec<float>("time");
@@ -1738,6 +1746,24 @@ void Analyze::Loop(NTupleReader& tr, int maxevents)
             {
                 if(regionsOfIntrest[k].passROI(x,y))
                 {
+                    if(counter[k] < max_save)
+                    {
+                        // Open a CSV file for writing
+                        // std::ofstream outputFile("waveform_"+regionsOfIntrest[k].getName()+"("+std::to_string(x)+","+std::to_string(y)+")_"+std::to_string(counter[k])+".csv");
+                        std::ofstream outputFile("waveform_"+regionsOfIntrest[k].getName()+"_"+std::to_string(counter[k])+".csv");
+                        // Write the header to the CSV file
+                        outputFile << "Time[ns],Channel1[mV],Channel2[mV],Channel3[mV],Channel4[mV],Channel5[mV],Channel6[mV],Channel7[mV]" << std::endl;
+                        for(unsigned int j = 0; j < time[0].size(); j++)
+                        {
+                            // auto t = timeCalibrationCorrection.at(i) + 80.0;
+                            outputFile<<1e9*time[0][j]<<","<<channel[0][j]<<","<<channel[1][j]<<","<<channel[2][j]<<","<<channel[3][j]<<","<<channel[4][j]<<","<<channel[5][j]<<","<<channel[6][j]<<std::endl;
+                        }
+                        // Close the CSV file
+                        outputFile.close();
+                        std::cout << "Data saved." << std::endl;
+                        counter[k]++;
+                    }
+
                     for(unsigned int i = 0; i < channel.size(); i++)
                     {
                         auto t = timeCalibrationCorrection.at(i) - 10.0;
